3b6813d264f0a3d06bf92aa39deb2511ba934290
nybux.tsui <nybux.tsui@gmail.com>
2013-07-17 08:40:25 +0800

    支持sort检索

--- finddict.sh
+++ finddict.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 R=`./kanji.py $1`
-cat dictionary/data* | awk -F'\t' -v r=$R '{if(match($1, r)) print $0;}'
+C=`cat dictionary/data* | awk -F'\t' -v r=$R '{if(match($1, r)) print $0;}' | wc -l`
+if [ $C = "0" ]; then
+    cat dictionary/sort* | awk -F'\t' -v r=$R '{if(match($1, r)) print $0;}'
+else
+    cat dictionary/data* | awk -F'\t' -v r=$R '{if(match($1, r)) print $0;}'
+fi
+
+
